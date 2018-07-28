3eb7b2c5dc9c6a137fab01eb80e3d22e37d32aee
Seongmin Park <me@komalab.io>
2018-07-28 23:34:35 +0900

    Sort by post list descending
    Signed-off-by: Seongmin Park <me@komalab.io>

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,10 +4,13 @@ var Post = require("../models/Post.js");
 
 // 게시글 리스트 가져오기
 router.get("/", function(req, res, next) {
-  Post.find(function(err, list) {
-    if (err) return next(err);
-    res.json(list);
-  });
+  Post.find()
+    // 내림차순 정렬 (최신글이 위로 올라오게끔)
+    .sort({ title: -1 })
+    .exec(function(err, list) {
+      if (err) return next(err);
+      res.json(list);
+    });
 });
 
 // 개별 게시글 가져오기
