08586fd6822218cd5c063ce66109614d9205230f
Seongmin Park <me@komalab.io>
2018-07-31 16:43:59 +0900

    Fix timezone in Server
    Signed-off-by: Seongmin Park <me@komalab.io>

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -4,7 +4,7 @@ var PostSchema = new mongoose.Schema({
   title: String,
   writer: String,
   content: String,
-  write_date: { type: Date, default: Date.now }
+  write_date: { type: Date, default: new Date().getTime() + 1000 * 60 * 60 * 9 }
 });
 
 module.exports = mongoose.model("Post", PostSchema);
